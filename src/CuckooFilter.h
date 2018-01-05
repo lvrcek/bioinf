69b08ca561a767036e36bc0417e9037cb0051e71
lvrcek <lovro.vrcek5@gmail.com>
2018-01-05 01:53:21 +0100

    Fingerprint optimization: int to bitset

--- src/CuckooFilter.h
+++ src/CuckooFilter.h
@@ -2,17 +2,16 @@
 #define BIOINF_CUCKOOFILTER_H
 
 #include <string>
+#include <bitset>
 #include "Table.h"
+#include "Constants.h"
 
-#define M 15      //number of buckets in a table
-#define B 8       //number of elements in a single bucket
-#define N 100     //total number of elements to be inserted in a bucket
-#define F 8       //size of fingerprint in bits
+
 static int MaxNumKicks = 150;
 
 int hashFunction(std::string word);
 
-int fingerprint(std::string word);
+std::bitset<F> fingerprint(std::string word);
 
 bool insertEntry(Table &table, std::string word);
 
