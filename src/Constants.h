47c03f7f64dba1d46f1f57c21da13b14347f4da0
lvrcek <lovro.vrcek5@gmail.com>
2018-01-05 01:57:46 +0100

    Moving declaration of constants to separate header

---
+++ src/Constants.h
@@ -0,0 +1,11 @@
+#ifndef BIOINF_CONSTANTS_H
+#define BIOINF_CONSTANTS_H
+
+
+#define M 15      //number of buckets in a table
+#define B 4       //number of slots in a single bucket
+#define N 100     //total number of elements to be inserted in a bucket
+#define F 8       //size of fingerprint in bits
+
+
+#endif //BIOINF_CONSTANTS_H
