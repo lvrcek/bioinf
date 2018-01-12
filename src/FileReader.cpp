34011374df74cc8c995d7125a166158679017f97
lmartinez <luka.martinez@asseco-see.hr>
2018-01-12 18:32:12 +0100

    added lookup of elements from table

--- src/FileReader.cpp
+++ src/FileReader.cpp
@@ -6,6 +6,38 @@
 //TODO problem with clion, need to set it to work on unix
 std::string fileName = "C:\\Users\\lmartinez\\Projects\\FER\\bioinf\\src\\resources\\Ecoli.fa";
 
+bool assertSimpleCuckooTable(Table &table, int k, int numEntries) {
+    std::ifstream input(fileName);
+    if (!input.good()) {
+        std::cout << "Error opening '" << fileName << "'." << std::endl;
+        return false;
+    }
+
+    std::string line, name, content;
+
+    std::getline(input, line);
+
+    while (std::getline(input, line).good()) {
+        if (line[0] == '>') break;
+        //std::cout << line <<std::endl;
+        content += line;
+        if (content.length() > numEntries + k - 1) break;
+    }
+
+    bool success;
+    for (int i = 0; i < numEntries; i++) {
+        std::string entry = content.substr(i, k);
+        success = lookupEntry(table, entry);
+        if (!success) {
+            std::cout << "Error while looking up entry " << i <<". Element "<< entry << " not found." << std::endl;
+            return false;
+        }
+    }
+
+    std::cout << "All entries successfully found in table." << std::endl;
+    return true;
+}
+
 Table *readSimpleCuckooTable(int k, int numEntries) {
 
     std::ifstream input(fileName);
@@ -42,7 +74,7 @@ Table *readSimpleCuckooTable(int k, int numEntries) {
         std::string entry = content.substr(i, k);
         success = insertEntry(*table, entry);
         if (!success) {
-            std::cout << "Error while inserting to table. Returning current table." << std::endl;
+            std::cout << "Error while inserting entry number " << i << " to table. Returning current table." << std::endl;
             return table;
         } else {
             std::cout << "Added entry: " << entry << std::endl;
